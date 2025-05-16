0f4a2fa27eae10fd8ee407af76de19ebf3ab9b23
hariansh-singh <harianshsingh13@gmail.com>
2025-05-16 17:47:12 +0530

    AdminDashboard APIS added

---
+++ backend/Controllers/AdminDashboardController.cs
@@ -0,0 +1,53 @@
+﻿using backend.Repositories.AdminDashboardRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminDashboardController : ControllerBase
+    {
+        private readonly IAdminDashboardRepo _adminDashRepo;
+
+        public AdminDashboardController(IAdminDashboardRepo dashboardRepository)
+        {
+            _adminDashRepo = dashboardRepository;
+        }
+
+        [HttpGet("total-users")]
+        public async Task<IActionResult> GetTotalUsers()
+        {
+            var data = await _adminDashRepo.GetTotalUsersAsync();
+            return Ok(new { err = 0, data = data });
+        }
+
+        [HttpGet("sellers-count")]
+        public async Task<IActionResult> GetSellersCount()
+        {
+            var data = await _adminDashRepo.GetSellersCountAsync();
+            return Ok(new { err = 0, data = data });
+        }
+
+        [HttpGet("new-orders")]
+        public async Task<IActionResult> GetNewOrders()
+        {
+            var data = await _adminDashRepo.GetNewOrdersAsync();
+            return Ok(new { err = 0, data = data });
+        }
+
+        [HttpGet("sales")]
+        public async Task<IActionResult> GetSalesRevenue([FromQuery] string period)
+        {
+            var data = await _adminDashRepo.GetSalesRevenueAsync(period);
+            return Ok(new { err = 0, data = data });
+        }
+
+        [HttpGet("top-products")]
+        public async Task<IActionResult> GetTopSellingProducts()
+        {
+            var data = await _adminDashRepo.GetTopSellingProductsAsync();
+            return Ok(new { err = 0, data = data });
+        }
+    }
+}
---
+++ backend/Repositories/AdminDashboardRepository/IAdminDashboardRepo.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Repositories.AdminDashboardRepository
+{
+    public interface IAdminDashboardRepo
+    {
+        Task<int> GetTotalUsersAsync();
+        Task<int> GetSellersCountAsync();
+        Task<int> GetNewOrdersAsync();
+        Task<decimal> GetSalesRevenueAsync(string period);
+        Task<List<object>> GetTopSellingProductsAsync();
+    }
+}
