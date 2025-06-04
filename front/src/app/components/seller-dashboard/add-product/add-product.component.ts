7796fac257423f0a1485e8c6dea89d0e40368169
Sapnacoder <sapnavkchaudhary@gmail.com>
2025-06-04 11:41:30 +0530

    sweet alert and navigation added after adding product

--- front/src/app/components/seller-dashboard/add-product/add-product.component.ts
+++ front/src/app/components/seller-dashboard/add-product/add-product.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductService } from '../../../../services/product.service';
 import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-product',
@@ -11,7 +13,7 @@ import { CommonModule } from '@angular/common';
 })
 export class AddProductComponent {
   selectedFile: File | null = null;
-    router: any;
+   // router: any;
 
   productForm: FormGroup = new FormGroup({
     ProductName: new FormControl('', [Validators.required]),
@@ -24,7 +26,7 @@ export class AddProductComponent {
   imageName: any;
 
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService,private router: Router) {}
 
   handleFileUpload(event: any) {
   if (event.target.files.length > 0) {
@@ -37,27 +39,41 @@ export class AddProductComponent {
     }
   }
 }
+submitProduct() {
+  if (!this.selectedFile) {
+    console.error("Please select an image.");
+    return;
+  }
 
+  const formData = new FormData();
+  formData.append("ProductName", this.productForm.value.ProductName);
+  formData.append("ProductPrice", this.productForm.value.ProductPrice);
+  formData.append("Description", this.productForm.value.Description);
+  formData.append("StockQuantity", this.productForm.value.StockQuantity);
+  formData.append("ProductImage", this.selectedFile, this.selectedFile.name);
 
-
-
-  submitProduct() {
-    if (!this.selectedFile) {
-      console.error("Please select an image.");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("ProductName", this.productForm.value.ProductName);
-    formData.append("ProductPrice", this.productForm.value.ProductPrice);
-    formData.append("Description", this.productForm.value.Description);
-    formData.append("StockQuantity", this.productForm.value.StockQuantity);
-    formData.append("ProductImage", this.selectedFile, this.selectedFile.name);
-
-   
   this.productService.addProduct(formData).subscribe({
     next: (response) => {
       console.log("Product added successfully", response);
+
+      // SweetAlert2 Notification
+      Swal.fire({
+        title: 'Product Added!',
+        text: 'Do you want to go to the dashboard or add another product?',
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonText: 'Go to Dashboard',
+        cancelButtonText: 'Add More Products'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.router.navigate(['/sellerdashboard']); // Navigate to Dashboard
+        } else {
+          this.productForm.reset(); // Clear Form
+          this.selectedFile = null;
+          this.imagePreview = undefined;
+          this.imageName = null;
+        }
+      });
     },
     error: (error) => console.error("Error adding product", error),
   });
